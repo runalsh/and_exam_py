e3e46e788ab0e9e47d966c91f97db5b9dc303491
runalsh <shaidullin2@gmail.com>
2022-01-08 19:58:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hi():
-    return 'its snake app! pshshshshshsh!!!!!!!!  ▄▄▀█▄
-▀▀▀██
-░▄██▀
-▀██▄█▀██▄▄███▀█▄'
+    return 'its snake app! pshshshshshsh!!!!!!!!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port = 8082)
